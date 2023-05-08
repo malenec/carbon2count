74f36787709e990f2757995b94c20e00fba2bdc4
malenec <90346924+malenec@users.noreply.github.com>
2023-05-08 14:49:49 +0200

    changes create mode

--- src/main/java/utils/SetupTestUsers.java
+++ src/main/java/utils/SetupTestUsers.java
@@ -14,7 +14,7 @@ public class SetupTestUsers {
 
     EntityManagerFactory emf = EMF_Creator.createEntityManagerFactory();
     EntityManager em = emf.createEntityManager();
-//
+
 //    // IMPORTAAAAAAAAAANT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 //    // This breaks one of the MOST fundamental security rules in that it ships with default users and passwords
 //    // CHANGE the three passwords below, before you uncomment and execute the code below

62227cb66fa5d6b337020aaf1944d61d9866c1ba
Israa1809 <din-israa@hotmail.com>
2023-05-09 09:05:21 +0200

    test space

--- src/main/java/entities/Hobby.java
+++ src/main/java/entities/Hobby.java
@@ -6,6 +6,7 @@ import java.util.List;
 @Entity
 @Table(name = "hobby")
 public class Hobby {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)

d2dc240c399b807b9b4a341d20327c37a1cc37fd
Martin Lorentzen <silverball1@gmail.com>
2023-05-12 09:41:03 +0200

    test refinement

--- src/test/java/facades/GroceryListFacadeTest.java
+++ src/test/java/facades/GroceryListFacadeTest.java
@@ -28,7 +28,6 @@ public class GroceryListFacadeTest {
     private static List<GroceryDTO> listWithGrocieries = new ArrayList<>();
 
 
-
     @BeforeAll
     public static void setUpClass() {
         emf = EMF_Creator.createEntityManagerFactoryForTest();
@@ -39,31 +38,42 @@ public class GroceryListFacadeTest {
     void setUp() {
         EntityManager em = emf.createEntityManager();
 
-        g1 = new Grocery("Ra00468","Ost", "Mælkeprodukter", "kg",  0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 1.2);
-        g2 = new Grocery("Ra00469","OstHaps", "Mælkeprodukter", "kg",  0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 1.8);
+        g1 = new Grocery("Ra00468", "Ost", "Mælkeprodukter", "kg", 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 1.2);
+        g2 = new Grocery("Ra00469", "OstHaps", "Mælkeprodukter", "kg", 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 1.8);
 
 
-       try {
-           em.getTransaction().begin();
-           //em.createNamedQuery("GroceryList.deleteAllRows").executeUpdate();
-           em.createNamedQuery("GroceryLine.deleteAllRows").executeUpdate();
-           //em.createNamedQuery("User.deleteAllRows").executeUpdate();
-           em.createNamedQuery("Grocery.deleteAllRows").executeUpdate();
-           em.persist(g1);
-           em.persist(g2);
-           em.persist(user1);
-           em.getTransaction().commit();
+        try {
+
+            em.getTransaction().begin();
+            System.out.println("før GroceryLine.deleteAllRows");
+            em.createNamedQuery("GroceryLine.deleteAllRows").executeUpdate();
+            System.out.println("før Grocery.deleteAllRows");
+            em.createNamedQuery("Grocery.deleteAllRows").executeUpdate();
+            System.out.println("før GroceryList.deleteAllRows");
+            em.createNamedQuery("GroceryList.deleteAllRows").executeUpdate();
+            System.out.println("før User.deleteAllRows");
+            em.createNamedQuery("User.deleteAllRows").executeUpdate();
+            System.out.println("efter alt er slettet");
+
+            em.persist(g1);
+            System.out.println("efter grocery persist");
+            em.persist(g2);
+            System.out.println("efter grocery persist2");
+            em.persist(user1);
+            System.out.println("efter user persist");
+
+            em.getTransaction().commit();
+            System.out.println("efter commit");
         } finally {
             em.close();
+            System.out.println("efter em closed");
         }
         groceryDTO1 = new GroceryDTO(g1);
         groceryDTO2 = new GroceryDTO(g2);
 
         userDTO1 = new UserDTO(user1);
 
-
-//        listWithGrocieries.add(groceryDTO1);
-//        listWithGrocieries.add(groceryDTO2);
+        System.out.println("til sidst i before each");
     }
 
 //    @Test
@@ -80,69 +90,8 @@ public class GroceryListFacadeTest {
 //        GroceryListDTO facadeGroceryList = facade.createGroceryList(userDTO1, groceryListDTO);
 //
 //        assertEquals("Ra00468", facadeGroceryList.getGroceryLineDTOs().get(0).getGroceryId());
+//
+//        System.out.println("create groceryList test done");
 //    }
 
-
-//    @Test
-//    void addQuote() {
-//        EntityManager em = emf.createEntityManager();
-//        QuoteDTO expectedQuote1 = new QuoteDTO(new Quote("jeg er sej"));
-//        QuoteDTO expectedQuote2 = new QuoteDTO(new Quote("jeg er mega sej"));
-//        expectedQuote1 = facade.createQuote(expectedQuote1);
-//        expectedQuote2 = facade.createQuote(expectedQuote2);
-//
-//        try {
-//            em.getTransaction().begin();
-//            user = em.find(User.class, user.getUserName());
-//            em.getTransaction().commit();
-//        } finally {
-//            em.close();
-//        }
-//
-//        facade.addQuote("user", expectedQuote1.getId());
-//        UserDTO userDTO = facade.addQuote("user", expectedQuote2.getId());
-//
-//        userDTO.getQuotes().forEach(quoteDTO -> System.out.println(quoteDTO.getQuote()));
-//
-//        assertEquals(expectedQuote2.getQuote(), userDTO.getQuotes().get(1).getQuote());
-//
-//    }
-//
-//    @Test
-//    void removeQuoteFromUser() {
-//        EntityManager em = emf.createEntityManager();
-//
-//        QuoteDTO expectedQuote1 = new QuoteDTO(new Quote("jeg er sej"));
-//        QuoteDTO expectedQuote2 = new QuoteDTO(new Quote("jeg er mega sej"));
-//
-//        expectedQuote1 = facade.createQuote(expectedQuote1);
-//        expectedQuote2 = facade.createQuote(expectedQuote2);
-//
-//        try {
-//            em.getTransaction().begin();
-//            user = em.find(User.class, user.getUserName());
-//            em.getTransaction().commit();
-//        } finally {
-//            em.close();
-//        }
-//
-//        facade.addQuote("user", expectedQuote1.getId());
-//        UserDTO userDTO = facade.addQuote("user", expectedQuote2.getId());
-//
-//        //checks what's in the list BEFORE remove method is called
-//        userDTO.getQuotes().forEach(quoteDTO -> System.out.println(quoteDTO.getQuote()));
-//
-//        int listSizeBeforeRemove = userDTO.getQuotes().size();
-//
-//        assertEquals(listSizeBeforeRemove, userDTO.getQuotes().size());
-//
-//        userDTO = facade.removeQuoteFromUser("user", expectedQuote1.getId());
-//
-//        //checks what's in the list AFTER remove method is called
-//        userDTO.getQuotes().forEach(quoteDTO -> System.out.println(quoteDTO.getQuote()));
-//
-//        assertEquals(false, userDTO.getQuotes().contains(expectedQuote1.getQuote()));
-//        assertEquals(listSizeBeforeRemove-1, userDTO.getQuotes().size());
-//
-//    }
 }
